6d1398d22ee0aade12ed8a4810097eedb9b973e4
Kedar V <kedar54@gmail.com>
2016-08-14 12:18:39 -0500

    add Excel import

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use \Excel;
+use App\Models\Application;
 
 class PageController extends Controller
 {
@@ -18,12 +20,46 @@ class PageController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Import new records in Excel sheet to database
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function importExcel()
     {
+        $excel = [];
+        Excel::load('resources/report.xlsx', function($reader) use (&$excel) {
+            $objExcel = $reader->getExcel();
+            $sheet = $objExcel->getSheet(0);
+            $highestRow = $sheet->getHighestRow();
+            $highestColumn = $sheet->getHighestColumn();
+
+            //  Loop through each row of the worksheet in turn
+            for ($row = 3; $row <= $highestRow; $row++)
+            {
+                //  Read a row of data into an array
+                $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row,
+                    NULL, TRUE, FALSE);
+
+                $excel[] = $rowData[0];
+                $application = \App\Models\Application::firstOrNew(['uuid' => $rowData[0][0], 'name' => $rowData[0][1]]);
+                // if($application->exists) {
+                //    echo "skipped: " . $row . "<br/>";
+                // } else {
+                //    echo "added: " . $row . "<br/>";
+                // }
+                $application->email = $rowData[0][2];
+                $application->q1 = $rowData[0][3];
+                $application->q2 = $rowData[0][4];
+                $application->q3 = $rowData[0][5];
+                $application->q4 = $rowData[0][6];
+                $application->q5 = $rowData[0][7];
+                $application->q6 = $rowData[0][8];
+                $application->save();
+            }
+        });
+    }
+
+    public function index() {
         return view('home');
     }
 }
---
+++ database/migrations/2016_08_14_162837_create_applications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uuid');
+            $table->string('name');
+            $table->string('email');
+            $table->text('q1');
+            $table->text('q2');
+            $table->text('q3');
+            $table->text('q4');
+            $table->text('q5');
+            $table->text('q6');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('applications');
+    }
+}
